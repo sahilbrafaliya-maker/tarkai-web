224766bd047f9040ea91980bc4f1dccaa35a9ce8
sahilbrafaliya-maker <sahil.b.rafaliya@gmail.com>
2026-02-18 12:51:45 +0530

    change mongo

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -27,6 +27,9 @@ async function dbConnect() {
     if (!cached.promise) {
         const opts = {
             bufferCommands: false,
+            serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+            socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+            family: 4 // Use IPv4, skip trying IPv6
         };
 
         cached.promise = mongoose.connect(MONGODB_URI!, opts).then((mongoose) => {
